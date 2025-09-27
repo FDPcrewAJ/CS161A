b7c64af2ed3300a9cc704fde8bcc2e6537ca7b81
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-09-27 16:49:10 -0700

    a01 is now functional

--- Assignments/A01/a01.cpp
+++ Assignments/A01/a01.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 using namespace std;
 
+// Main Function
 int main() {
     
-    cout << "hello world!" << endl;
+    // Initialize variables needed for input
+    int employeeId = 0;
+    int hoursWorked = 0;
+    int hourlyRate = 0;
+    int withholdRate = 0;
+    // Initialize variables needed for calculations
+    int grossPay = 0;
+    int taxWithheld = 0;
+    int netPay = 0;
+
+    // Display welcome messsage
+    cout << "Weekly Payroll Calculator (Input whole numbers only)" << endl;
+    
+    // Get user input data
+    cout << "Enter your employee ID number: ";
+    cin >> employeeId;
+    cout << "Enter number of hours worked: ";
+    cin >> hoursWorked;
+    cout << "Enter your hourly rate: ";
+    cin >> hourlyRate;
+    cout << "Enter the federal withholding rate: ";
+    cin >> withholdRate;
+
+    // Calculate gross pay, tax withheld, and net pay
+    grossPay = hoursWorked * hourlyRate;
+    taxWithheld = withholdRate / 100.0 * grossPay;
+    netPay = grossPay - taxWithheld;
+
+    // Display calculated pay results
+    cout << "Payroll Summary" << endl;
+    cout << "Total Gross Pay: $" << grossPay << endl;
+    cout << "Federal Tax Withholding: $" << taxWithheld << endl;
+    cout << "Net Pay: $" << netPay << endl;
+    cout << "Thank you for using the Weekly Payroll Calculator!" << endl;
     
     return 0;
 }
