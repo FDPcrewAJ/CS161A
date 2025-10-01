20f50c5bd6d5b3d314e9d4a06eec1ea5097297ea
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-10-01 13:18:10 -0700

    a02 is now working

--- Assignments/A02/a02.cpp
+++ Assignments/A02/a02.cpp
@@ -8,7 +8,25 @@ const int CALORIES_PER_SERVING = 160;
 // Main funciton
 int main() {
     // Variable setup
-    
+    int numOreos = 0;
+    float numServings = 0.0;
+    float numCalroies = 0.0;
+
+    // Welcome Message and oreos eaten input
+    cout << "Oreo Calculator" << endl;
+    cout << endl << "Enter the number of Oreos eaten: ";
+    cin >> numOreos;
+
+    // Calculate number of servings
+    numServings = static_cast<double>(numOreos) / OREOS_PER_SERVING;
+    // Calculate number of calroies eaten
+    numCalroies = numServings * CALORIES_PER_SERVING;
+
+    // Display results
+    cout << endl << "Number of servings eaten: " << numServings << endl;
+    cout << "Number of calories consumed: " << numCalroies << endl;
+    // Goodbye message
+    cout << endl << "Thank you for using!" << endl;
 
     return 0;
 }
