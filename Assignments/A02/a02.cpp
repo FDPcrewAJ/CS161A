ad4f1ddff911ef830b56442dbb9bf39bfc11840e
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-10-01 13:04:02 -0700

    added a02.cpp file

---
+++ Assignments/A02/a02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Constant variables
+const int OREOS_PER_SERVING = 5;
+const int CALORIES_PER_SERVING = 160;
+
+// Main funciton
+int main() {
+    // Variable setup
+    
+
+    return 0;
+}
