fb135f090d9d6410d5c19652a00c7de27aabf69f
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-11-01 22:53:58 -0700

    finished a05

--- Assignments/A05/a05.cpp
+++ Assignments/A05/a05.cpp
@@ -1,9 +1,37 @@
+/**********************************************************************
+# Author:       Aiden Jungels
+# Assignment:   A05 (CS161A)
+# Date:         11/1/2025
+# Description:  This program will ask the user for the number of 
+#               people attending a party, average number of slices per 
+#               person, and the cost of a pizza, then calculate the 
+#               total cost of the pizza with tax and delivery included, 
+#               in a do while loop, allowing the user to enter data for 
+#               as many parties as they choose. Once done entering data, 
+#               the program will display the number of entries, total 
+#               number of pizzas, average number of pizzas between all 
+#               parties, and maximum number of people and pizza cost 
+#               of a party.
+# Input:        int numPeople, float avgSlices, float perPizzaCost,
+#               char userTermination
+# Output:       In each while iteration: outputs the number of pizzas
+#               needed, cost of pizzas, cost of tax, cost of delivery
+#               and total cost
+#               Once while loop is exited: outputs the number of
+#               parties, total number of pizzas from all parties,
+#               average number of pizzas for all parties, max number
+#               of people of all parties, and max cost of pizza of 
+#               all parties
+# Sources:      CS Assigment 5 Doc, CS1 Algorithmic Design Doc,
+#               Zybooks Chapter 5
+**********************************************************************/
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 
 // Static variables
-static int NUM_SLICES = 8;
+static int SLICES_PER_PIZZA = 8;
 static float SALES_TAX = 0.07;
 static float DELIVERY_FEE = 0.20;
 
@@ -18,20 +46,20 @@ int main() {
     char userTermination = ' ';
     // In loop output vars
     int numPizzasPerParty = 0;
-    float pizzaCost = 0.0;
+    float partyPizzaCost = 0.0;
     float taxCost = 0.0;
     float deliveryCost = 0.0;
     float singlePartyCost = 0.0;
     // Accumulator vars
     int numParties = 0;
-    int totalPizzas = 0;
     // Totals output vars
+    int totalPizzas = 0;
     float avgNumPizzas = 0.0;
     int maxNumPeople = 0;
-    float avgPizzaCost = 0.0;
+    float maxPizzaCost = 0.0;
 
     // Welcome message
-    cout << "Welcome to the Pizza Part Statistics Calculator!" << endl;
+    cout << "Welcome to the Pizza Party Statistics Calculator!" << endl;
 
     // Do while loop to get user party input
     do {
@@ -45,22 +73,30 @@ int main() {
         
         // Calulate pizzas and prices
         // Calculate number of pizzas needed
-
+        numPizzasPerParty = ceil((numPeople * avgSlices) / SLICES_PER_PIZZA);
         // Calculate cost of pizzas
-
+        partyPizzaCost = numPizzasPerParty * perPizzaCost;
         // Calculate tax
-
+        taxCost = partyPizzaCost * SALES_TAX;
         // Calculate delivery
-
+        deliveryCost = (partyPizzaCost + taxCost) * DELIVERY_FEE;
         // Calculate total cost
-
+        singlePartyCost = partyPizzaCost + taxCost + deliveryCost;
+        
         // Determine if any maximums
-
+        if (numPeople > maxNumPeople) {
+            maxNumPeople = numPeople;
+        }
+        if (singlePartyCost > maxPizzaCost) {
+            maxPizzaCost = singlePartyCost;
+        }
+        
         // Display results for a single party
+        cout << fixed << setprecision(2);
         cout << setw(17) << left << endl << "Number of pizzas:" << " ";
         cout << numPizzasPerParty << endl;
         cout << setw(17) << left << "Cost of pizzas:" << "$";
-        cout << pizzaCost << endl;
+        cout << partyPizzaCost << endl;
         cout << setw(17) << left << "Tax:" << "$";
         cout << taxCost << endl;
         cout << setw(17) << left << "Delivery:" << "$";
@@ -79,7 +115,20 @@ int main() {
     } 
     while (userTermination == 'y');
 
-    cout << numParties;
+    // Calculate averages
+    avgNumPizzas = static_cast<float>(totalPizzas) / numParties;
+    
+    // Display totals
+    cout << endl << "Number of parties: " << numParties << endl;
+    cout << "Total number of pizzas: " << totalPizzas << endl;
+    cout << fixed << setprecision(1);
+    cout << "Average number of pizzas: " << avgNumPizzas << endl;
+    cout << "Maximum number of people: " << maxNumPeople << endl;
+    cout << fixed << setprecision(2);
+    cout << "Maximum cost of pizzas: $" << maxPizzaCost << endl;
+
+    // Goobye message
+    cout << endl << "Thank you for using the Pizza Party Statistics Calculator!" << endl;
 
     return 0;
 }
