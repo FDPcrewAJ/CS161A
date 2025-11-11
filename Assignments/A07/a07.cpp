9725ed8ef5ebca75a26ebe2b792fe552d291b406
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-11-11 12:56:35 -0800

    started working on a07

---
+++ Assignments/A07/a07.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Read integer function prototype
+int readInt(string prompt);
+
+// Read double score function prototype
+double readScore(string prompt);
+
+// Assign averages function prototype
+double assignaverage(int numAssigns);
+
+// Calculate number grade function prototype
+double calcFinalScore(double assignAvg, double midterm, double final);
+
+// Calulate letter grade function prototype
+char calcLetterGrade(double finalScore);
+
+// Main Function
+int main() {
+    return 0;
+}
+
+// Read integer function
+int readInt(string prompt) {
+    return 0;
+}
+
+// Read double score function
+double readScore(string prompt) {
+    return 0.0;
+}
+
+// Assign averages function
+double assignaverage(int numAssigns) {
+    return 0.0;
+}
+
+// Calculate number grade function
+double calcFinalScore(double assignAvg, double midterm, double final) {
+    return 0.0;
+}
+
+// Calulate letter grade function
+char calcLetterGrade(double finalScore) {
+    return ' ';
+}
